ba4c37c66e5bed51c52c81d8e9911e9dc184e3bb
marat <marat.zamlet-97@yandex.ru>
2025-06-09 15:57:44 +0400

    fixed swiper

--- source/js/vendor/screens-swiper.js
+++ source/js/vendor/screens-swiper.js
@@ -1,13 +1,15 @@
 import Swiper from "swiper/bundle";
 import {Navigation} from "swiper/modules";
+import 'swiper/css';
 
 const swiperScreens = new Swiper('.screens__swiper', {
   direction: 'horizontal',
   slidesPerView: 1,
+  effect: "fade",
   loop: true,
   mousewheel: true,
+  autoHeight: true,
   keyboard: true,
-  spaceBetween: 40, 
   modules: [Navigation],
   navigation: {
     nextEl: '.screens-next',
