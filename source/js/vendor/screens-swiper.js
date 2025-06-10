20dcda89efc61ed88760f4121f48b878a6b6b8fb
marat <marat.zamlet-97@yandex.ru>
2025-06-10 16:26:44 +0400

    working slidees

--- source/js/vendor/screens-swiper.js
+++ source/js/vendor/screens-swiper.js
@@ -3,18 +3,27 @@ import {Navigation} from "swiper/modules";
 import 'swiper/css';
 
 const swiperScreens = new Swiper('.screens__swiper', {
-  direction: 'horizontal',
+  // direction: 'horizontal',
   slidesPerView: 1,
+  slidesPerGroup:1,
   effect: "fade",
-  loop: true,
-  mousewheel: true,
-  autoHeight: true,
+  // loop: true,
+  // mousewheel: true,
+  // autoHeight: true,
   keyboard: true,
   modules: [Navigation],
   navigation: {
     nextEl: '.screens-next',
     prevEl: '.screens-prev',
     },
+    watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  slideToClickedSlide: true,
+  // speed: 400, // скорость анимации
+  // fadeEffect: {
+  //   crossFade: true
+  // }
 });
 
 export default swiperScreens;
